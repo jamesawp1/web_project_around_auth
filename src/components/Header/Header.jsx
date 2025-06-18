40ff807d3fba6be941709321f470aaf4c39019f0
jamesawp1 <jamesawp@protonmail.com>
2025-06-18 17:23:43 -0300

    feat: Add a menu in mobile version

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,36 @@
 import logo from "../../images/header__image.png";
+import menuLogo from "../../images/menuLogo.svg";
+import { useRef } from "react";
 
 export default function Header({ emailText, onLogout }) {
+  const headerRef = useRef();
+
+  const handleChange = (evt) => {
+    const isChecked = evt.target.checked;
+
+    headerRef.current.style.marginTop = isChecked ? "142px" : "45px";
+  };
+
   return (
-    <header className="header">
+    <header className="header" ref={headerRef}>
       <img
         className="header__image"
         src={logo}
         alt="Logo do site Around The Us."
       />
+      <input
+        type="checkbox"
+        id="menu-toggle"
+        className="header__menu"
+        onChange={handleChange}
+      />
+      <label htmlFor="menu-toggle" className="header__menu_label">
+        <img
+          className="header__menu_label-icon"
+          alt="Logo do menu."
+          src={menuLogo}
+        />
+      </label>
       <div className="header__wrapper">
         <h3
           className={emailText ? "header__message" : "header__message-hidden"}
